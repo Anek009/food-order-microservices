2919e5d63dd71b48631e50073980eb032fb0320b
Anek009 <aneksekhar2347@gmail.com>
2025-04-11 11:13:04 +0530

    Add files via upload

---
+++ food-eureka/src/main/java/com/example/food_eureka/FoodEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodEurekaApplication.class, args);
+	}
+
+}
---
+++ food-restaurant/food-restaurant/src/test/java/com/example/food_restaurant/FoodRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
